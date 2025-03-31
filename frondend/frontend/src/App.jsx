81752a63170ebbd8e8f0f216f2e4a2e5aa57d3a9
ReyolJanu <ignatious.janukshan20@gmail.com>
2025-03-31 10:30:30 +0530

    frontend setup

---
+++ frondend/frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./commponents/layouts/Header"
+import './App.css'
+import Footer from "./commponents/layouts/Footer"
+import Home from "./commponents/Home"
+
+function App() {
+
+  return (
+    <>
+     <Header/>
+     <Footer/>
+     <Home/>
+    </>
+  )
+}
+
+export default App
---
+++ frondend/frontend/src/commponents/layouts/Footer.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='App'>
+      <footer class="py-1">
+      <p class="text-center text-dark mt-1">
+        JVLcart - 2022-2023, All Rights Reserved
+      </p>
+    </footer>
+    </div>
+  )
+}
+
+export default Footer
